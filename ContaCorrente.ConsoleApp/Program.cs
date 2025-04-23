3f074f5c1328078b53be6ea0110ba2a697485190
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-04-23 14:46:19 -0300

    criará métodos de depósito e saque

--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -4,29 +4,48 @@ namespace ContaCorrente.ConsoleApp
 {
     internal class Program
     {
-        //Além disso, a conta mantém um registro de movimentações financeiras, que englobam todas as transações realizadas.
         static void Main(string[] args)
         {
             CriarConta conta1 = new CriarConta();
-            //conta1.numeroId += 1;
-            conta1.saldo = 1000.0;
-            //conta1.limiteDebito = 500;
-            Console.WriteLine($"ID: {conta1.numeroConta}\nSaldo: {conta1.saldo}\nLimite de Débito: {conta1.limiteDebito}");
+ 
+            Operacoes.Depositar(conta1, -300);
+            Console.WriteLine($"Conta Corrente: {conta1.numeroConta}\nSaldo: {conta1.saldo}\nLimite de Débito: {conta1.limiteDebito}");
             Console.WriteLine("------------------------------------------------------------");
 
             CriarConta conta2=new CriarConta();
-            Console.WriteLine($"ID: {conta2.numeroConta}\nSaldo: {conta2.saldo}\nLimite de Débito: {conta2.limiteDebito}");
+            Operacoes.Depositar(conta2, 500);
+            Operacoes.Sacar(conta2, 1000);
+            Console.WriteLine($"Conta Corrente: {conta2.numeroConta}\nSaldo: {conta2.saldo}\nLimite de Débito: {conta2.limiteDebito}");
             Console.ReadLine();
         }
     }
     public class CriarConta()
     {
-        //número de identificação único,
         static int numeroId = 1000;
         public int numeroConta =numeroId++;
-        //um saldo disponível
         public double saldo = 0;
-        //e um limite de débito estabelecido.
         public int limiteDebito = 0;
+    }
+
+    public static class Operacoes
+    {
+        public static void Depositar(CriarConta conta, double valor)
+        {
+            if (valor < conta.limiteDebito)
+            {
+                Console.WriteLine("Valor insufuciente para depósito!");
+                return;
+            }
+            conta.saldo += valor;
+        }
+        public static void Sacar(CriarConta conta, double valor)
+        {
+            if (valor > conta.limiteDebito) 
+            {
+                Console.WriteLine("Saldo insufuciente para saque!");
+                return;
+            } 
+            conta.saldo -= valor;
+        }
     }
 }
