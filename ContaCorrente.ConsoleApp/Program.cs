a2c49007e995cd8903211b25e84ee0d2c290cad2
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-04-23 16:02:24 -0300

    criará método emitir extrato

--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -10,11 +10,13 @@ namespace ContaCorrente.ConsoleApp
  
             Operacoes.Depositar(conta1, -300);
             Operacoes.ConsultarSaldo(conta1);
+            Operacoes.EmitirExtrato(conta1);
 
             CriarConta conta2=new CriarConta();
             Operacoes.Depositar(conta2, 500);
             Operacoes.Sacar(conta2, 1000);
             Operacoes.ConsultarSaldo(conta2);
+            Operacoes.EmitirExtrato(conta2);
             Console.ReadLine();
         }
     }
@@ -24,33 +26,76 @@ namespace ContaCorrente.ConsoleApp
         public int numeroConta =numeroId++;
         public double saldo = 0;
         public int limiteDebito = 0;
+        public string[] extrato = new string[10];
+        public int contadorExtrato = 0;
     }
 
     public static class Operacoes
     {
-        public static void Depositar(CriarConta conta, double valor)
+        public static string Depositar(CriarConta conta, double valor)
         {
+            string mensagem;
             if (valor < conta.limiteDebito)
             {
-                Console.WriteLine("Valor insufuciente para depósito!");
-                return;
+                mensagem = $"Valor insufuciente para depósito![R${valor}]";
+                Console.WriteLine(mensagem);
+                conta.extrato[conta.contadorExtrato] += Convert.ToString(mensagem);
+                conta.contadorExtrato++;
+                return mensagem;
             }
             conta.saldo += valor;
+            mensagem = $"Depósito de R${valor} realizado com sucesso!";
+            Console.WriteLine(mensagem);
+            conta.extrato[conta.contadorExtrato] += Convert.ToString(mensagem);
+            conta.contadorExtrato++;
+            return mensagem;
+            
         }
-        public static void Sacar(CriarConta conta, double valor)
+        public static string Sacar(CriarConta conta, double valor)
         {
+            string mensagem;
             if (valor > conta.limiteDebito) 
             {
-                Console.WriteLine("Saldo insufuciente para saque!");
-                return;
+                mensagem = $"Saldo insufuciente para saque![R${valor}]";
+                Console.WriteLine(mensagem);
+                conta.extrato[conta.contadorExtrato] += Convert.ToString(mensagem);
+                conta.contadorExtrato++;
+                return mensagem;
             } 
             conta.saldo -= valor;
+            mensagem = $"Saque de R$ {valor} realizado com sucesso!";
+            Console.WriteLine(mensagem);
+            conta.extrato[conta.contadorExtrato] += Convert.ToString(mensagem);
+            conta.contadorExtrato++;
+            return mensagem;
         }
 
         public static void ConsultarSaldo(CriarConta conta)
         {
-            Console.WriteLine($"Conta Corrente: {conta.numeroConta}\nSaldo: {conta.saldo}\nLimite de Débito: {conta.limiteDebito}");
-            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("\n--------------- Saldo da conta ---------------".ToUpper());
+            Console.WriteLine($"Conta Corrente: {conta.numeroConta}\tSaldo: {conta.saldo}\tLimite de Débito: {conta.limiteDebito}");
+            Console.WriteLine("--------------- Fim do Saldo da conta ---------------\n".ToUpper());
+        }
+
+        public static void EmitirExtrato(CriarConta conta)
+        {
+            Console.WriteLine("\n--------------- Extrato da conta ---------------".ToUpper());
+            Console.WriteLine($"Conta Corrente: {conta.numeroConta}");
+
+            if (conta.contadorExtrato == 0) 
+            {
+                Console.WriteLine("Sem movimentações na conta ainda!"); 
+                return;
+            }
+
+            for(int i = 0; i < conta.contadorExtrato; i++)
+                {
+                if (!string.IsNullOrEmpty(conta.extrato[i]))
+                    {
+                        Console.WriteLine($" - {conta.extrato[i]}");
+                    }
+                }
+            Console.WriteLine("--------------- Fim do Extrato da conta ---------------\n".ToUpper());
         }
     }
 }
