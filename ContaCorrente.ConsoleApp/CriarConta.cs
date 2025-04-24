e8df1fc5e1d7518c469a08748422a14452ff1bf2
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-04-24 13:54:57 -0300

    separará as classes em arquivos

---
+++ ContaCorrente.ConsoleApp/CriarConta.cs
@@ -0,0 +1,12 @@
+﻿namespace ContaCorrente.ConsoleApp
+{
+    public class CriarConta()
+    {
+        static int numeroId = 1000;
+        public int numeroConta = numeroId++;
+        public double saldo = 0;
+        public int limiteDebito = 0;
+        public string[] extrato = new string[10];
+        public int contadorExtrato = 0;
+    }
+}
